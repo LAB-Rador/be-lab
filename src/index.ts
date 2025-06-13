ef66a943d79f5a656b34a8bd05a21ade1c31623c
egorkolev <kolevegor@gmail.com>
2025-06-13 18:44:16 -0400

    railway deploy

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,12 @@ import userLaboratoryRouter from './routes/userLaboratory.router';
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.options('*', cors());
+
 app.use(
     cors({
         origin: [
-            process.env.FRONTEND_URL as string || 'https://lab-rador-assist.vercel.app',
+            process.env.FRONTEND_URL || 'https://lab-rador-assist.vercel.app',
         ],
         credentials: true, // Allow cookies and auth headers
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
