6f453227d11c2025fcaf40d321ef35f54a0150da
egorkolev <kolevegor@gmail.com>
2025-06-14 18:47:35 -0400

    railway deploy

--- src/controllers/auth/login.controller.ts
+++ src/controllers/auth/login.controller.ts
@@ -31,10 +31,10 @@ export const login = async (req: Request, res: Response) => {
                 contactPhone: user.contactPhone,
                 address: user.address,
             },
-            process.env.PRIVATE_KEY!.replace(/\\n/g, '\n'),
+            process.env.PRIVATE_KEY!,
             { expiresIn: '1d' },
         );
-
+        // .replace(/\\n/g, '\n')
         const refreshToken = jwt.sign(
             {
                 userId: user.id,
@@ -47,7 +47,7 @@ export const login = async (req: Request, res: Response) => {
                 contactPhone: user.contactPhone,
                 address: user.address,
             },
-            process.env.PRIVATE_KEY!.replace(/\\n/g, '\n'),
+            process.env.PRIVATE_KEY!,
             { expiresIn: '7d' },
         );
 
