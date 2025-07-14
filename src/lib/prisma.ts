e60868b525e1045dfc98082d3cb30d941773feac
egorkolev <kolevegor@gmail.com>
2025-07-14 16:04:16 -0600

    Добавление логики для корректного завершения работы сервера и улучшение управления подключением PrismaClient в зависимости от окружения.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,22 @@
 import { PrismaClient } from '@prisma/client';
 
-const prismaClient = new PrismaClient();
+declare global {
+  var __prisma: PrismaClient | undefined;
+}
+
+let prismaClient: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+    prismaClient = new PrismaClient({
+    log: ['error'],
+  });
+} else {
+  if (!global.__prisma) {
+    global.__prisma = new PrismaClient({
+      log: ['error', 'warn'],
+    });
+  }
+  prismaClient = global.__prisma;
+}
 
 export default prismaClient;
